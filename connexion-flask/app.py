af4db7a1f332461ca12554816dc82a4daf442ee5
kanchan <kanchansingh1123@gmail.com>
2024-04-06 20:49:37 +0530

    First API using Connexion including swagger-ui.

--- connexion-flask/app.py
+++ connexion-flask/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import connexion
 
-app = Flask(__name__)
+app = connexion.FlaskApp(__name__, specification_dir="./")
+app.add_api("swagger.yml")
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001)
